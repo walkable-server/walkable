225ad654e7ff329b92e38b71b3fcd1a24c62e464
Hoàng Minh Thắng <p@banphim.net>
2018-02-22 22:48:32 +0700

    move some deps to dev deps

--- project.clj
+++ project.clj
@@ -3,20 +3,8 @@
   :url "https://github.com/walkable-server/walkable"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
                  [clojure-future-spec "1.9.0-beta4"]
-                 [duct/core "0.6.2"]
-                 [duct/module.logging "0.3.1"]
-                 [duct/logger.timbre "0.4.1"]
-                 [duct/module.web "0.6.4"]
-                 [duct/module.ataraxy "0.2.0"]
-                 [duct/module.cljs "0.3.1" :exclusions [org.clojure/clojurescript]]
-                 [duct/server.figwheel "0.2.1" :exclusions [org.clojure/clojurescript]]
-                 [devcards "0.2.1" :exclusions [org.clojure/clojurescript]]
-                 [duct/module.sql "0.4.2"]
-                 [com.wsscode/pathom "2.0.0-beta1"]
-                 [fulcrologic/fulcro "2.2.0" :exclusions [org.clojure/clojurescript]]
-                 [fulcrologic/fulcro-spec "2.0.1" :scope "test" :exclusions [fulcrologic/fulcro]]]
+                 [com.wsscode/pathom "2.0.0-beta1"]]
   :plugins [[duct/lein-duct "0.10.6"]]
   :main ^:skip-aot walkable-demo.main
   :resource-paths ["resources" "target/resources"]
@@ -31,8 +19,24 @@
    :profiles/dev {}
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
-                  :dependencies   [[integrant/repl "0.2.0"]
-                                   [eftest "0.4.0"]
-                                   [org.xerial/sqlite-jdbc "3.21.0.1"]
+                  :dependencies   [[duct/core "0.6.2"]
+                                   [duct/module.logging "0.3.1"]
+                                   [duct/logger.timbre "0.4.1"]
+                                   [duct/module.web "0.6.4"]
+                                   [duct/module.ataraxy "0.2.0"]
+
+                                   [duct/module.sql "0.4.2"]
                                    [duct/database.sql.hikaricp "0.3.2"]
+                                   [org.xerial/sqlite-jdbc "3.21.0.1"]
+
+                                   [org.clojure/clojurescript "1.9.946"]
+                                   [duct/module.cljs "0.3.2" :exclusions [org.clojure/clojurescript]]
+                                   [duct/server.figwheel "0.2.1" :exclusions [org.clojure/clojurescript]]
+                                   [devcards "0.2.4" :exclusions [org.clojure/clojurescript]]
+
+                                   [fulcrologic/fulcro "2.2.0" :exclusions [org.clojure/clojurescript]]
+                                   [fulcrologic/fulcro-spec "2.0.2" :scope "test" :exclusions [fulcrologic/fulcro]]
+
+                                   [integrant/repl "0.3.0"]
+                                   [eftest "0.4.3"]
                                    [kerodon "0.9.0"]]}})
