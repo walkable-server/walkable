f1e2b1458c6bea1efbcaae5f8041c2d43704884a
Hoàng Minh Thắng <p@banphim.net>
2018-02-23 15:33:36 +0700

    more examples

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -54,13 +54,76 @@
   (q "SELECT * from foo")
   (e "DROP TABLE `foo`")
   )
+
+(defn run-print-query [& xs]
+  (let [[q & args] (rest xs)]
+    (println q)
+    (println args))
+  (apply jdbc/query xs))
+
+;; simple join examples
+#_
+(let [eg-1
+      '[{[:farmer/by-id 1] [:farmer/number :farmer/name
+                            :farmer/cow-index
+                            {:farmer/cow [:cow/index :cow/color]}]}]
+
+      parser
+      example/pathom-parser]
+  (parser {::sqb/sql-db    (db)
+           ::sqb/run-query run-print-query
+
+           ::sqb/sql-schema
+           (sqb/compile-schema
+             {:columns          [:cow/index
+                                 :cow/color
+                                 :farmer/cow-index
+                                 :farmer/number
+                                 :farmer/name]
+              :idents           {:farmer/by-id [:= :farmer/number]
+                                 :farmers/all  "farmer"}
+              :extra-conditions {}
+              :joins            {:farmer/cow [:farmer/cow-index :cow/index]}
+              :reversed-joins   {:cow/owner :farmer/cow}
+              :join-cardinality {:farmer/by-id :one
+                                 :cow/owner    :one
+                                 :farmer/cow   :one}})}
+    eg-1))
+
+#_
+(let [eg-1
+      '[{[:kid/by-id 1] [:kid/number :kid/name
+                         {:kid/toy [:toy/index :toy/color]}]}]
+
+      parser
+      example/pathom-parser]
+  (parser {::sqb/sql-db    (db)
+           ::sqb/run-query run-print-query
+
+           ::sqb/sql-schema
+           (sqb/compile-schema
+             {:columns          [:kid/number :kid/name :toy/index :toy/color :toy/owner-number]
+              :idents           {:kid/by-id [:= :kid/number]
+                                 :kids/all  "kid"}
+              :extra-conditions {}
+              :joins            {:toy/owner [:toy/owner-number :kid/number]}
+              :reversed-joins   {:kid/toy :toy/owner}
+              :join-cardinality {:kid/by-id :one
+                                 :kid/toy   :one
+                                 :toy/owner :one}})}
+    eg-1))
+
+;; filters and conditions example
 #_
 (let [eg-1
       '[{(:people/all {::sqb/filters  {:person/number [:< 10]}
-                       ::sqb/limit    1
-                       ::sqb/offset   1
+                       ;; ::sqb/limit    1
+                       ;; ::sqb/offset   1
                        ::sqb/order-by [[:person/name :desc]]})
-         [:person/number :person/name]}]
+         [:person/number :person/name
+          {:person/pet [:pet/index
+                        :pet/age
+                        :pet/color]}]}]
 
       eg-2
       '[{[:person/by-id 1]
@@ -73,14 +136,11 @@
                         {:pet/owner [:person/name]}]}]}]
       parser
       example/pathom-parser]
-  (parser {:current-user 1
-           ::sqb/sql-db  (db)
-           ::sqb/run-query
-           (fn [& xs]
-             (let [[q & args] (rest xs)]
-               (println q)
-               (println args))
-             (apply jdbc/query xs))
+  (parser {;; extra env data, eg current user id provided by Ring session
+           :current-user   1
+
+           ::sqb/sql-db    (db)
+           ::sqb/run-query run-print-query
            ::sqb/sql-schema
            (sqb/compile-schema
              ;; which columns are available in SQL table?
