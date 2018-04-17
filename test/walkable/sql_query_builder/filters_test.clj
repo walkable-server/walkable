44fe22b7f8061b998e013206f575ba8483ac918f
Hoàng Minh Thắng <p@banphim.net>
2018-04-18 00:27:32 +0700

    add inline-params test

--- test/walkable/sql_query_builder/filters_test.clj
+++ test/walkable/sql_query_builder/filters_test.clj
@@ -2,3 +2,12 @@
   (:require [walkable.sql-query-builder.filters :as sut]
             [clojure.spec.alpha :as s]
             [clojure.test :as t :refer [deftest testing is]]))
+
+(deftest inline-params-tests
+  (is (= (sut/inline-params
+           {:raw-string " ? "
+            :params     [{:raw-string "2018 - `human`.`yob`"
+                          :params     []}]})
+        {:raw-string " 2018 - `human`.`yob` "
+         :params     []})))
+
