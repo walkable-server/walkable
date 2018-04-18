f6c12acb74a413553cbd58dc652349108ee9a7ff
Hoàng Minh Thắng <p@banphim.net>
2018-04-19 01:02:30 +0700

    add lots of operator tests

--- test/walkable/sql_query_builder/filters_test.clj
+++ test/walkable/sql_query_builder/filters_test.clj
@@ -11,3 +11,67 @@
         {:raw-string " 2018 - `human`.`yob` "
          :params     []})))
 
+(deftest and-tests
+  (is (= (sut/process-operator {} [:and []])
+        {:raw-string "(?)",
+         :params [{:raw-string " ? ", :params [true]}]}))
+  (is (= (sut/process-operator {} [:and [{}]])
+        {:raw-string "(?)", :params [{}]}))
+  (is (= (sut/process-operator {} [:and [{} {}]])
+        {:raw-string "(?) AND (?)", :params [{} {}]}))
+  (is (= (sut/process-operator {} [:and [{} {} {}]])
+        {:raw-string "(?) AND (?) AND (?)", :params [{} {} {}]})))
+
+(deftest or-tests
+  (is (= (sut/process-operator {} [:or []])
+        {:raw-string "NULL", :params []}))
+  (is (= (sut/process-operator {} [:or [{}]])
+        {:raw-string "(?)", :params [{}]}))
+  (is (= (sut/process-operator {} [:or [{} {}]])
+        {:raw-string "(?) OR (?)", :params [{} {}]}))
+  (is (= (sut/process-operator {} [:or [{} {} {}]])
+        {:raw-string "(?) OR (?) OR (?)", :params [{} {} {}]})))
+
+(deftest in-tests
+  (is (= (sut/process-operator {} [:in []])
+        {:raw-string "(?) IN ()", :params []}))
+  (is (= (sut/process-operator {} [:in [{}]])
+        {:raw-string "(?) IN ()", :params [{}]}))
+  (is (= (sut/process-operator {} [:in [{} {}]])
+        {:raw-string "(?) IN (?)", :params [{} {}]}))
+  (is (= (sut/process-operator {} [:in [{} {} {}]])
+        {:raw-string "(?) IN (?, ?)", :params [{} {} {}]})))
+
+(deftest not-tests
+  (is (= (sut/process-operator {} [:not [{}]])
+        {:raw-string "NOT (?)", :params [{}]})))
+
+(deftest =-tests
+  ;; the same to >, >=, <, <=
+  (is (= (sut/process-operator {} [:= [{} {}]])
+        {:raw-string "(?)=(?)", :params ({} {})}))
+  (is (= (sut/process-operator {} [:= [{} {} {}]])
+        {:raw-string "(?)=(?) AND (?)=(?)", :params ({} {} {} {})}))
+  (is (= (sut/process-operator {} [:= [{} {} {} {}]])
+        {:raw-string "(?)=(?) AND (?)=(?) AND (?)=(?)", :params ({} {} {} {} {} {})})))
+
+(deftest +-and-the-like-tests
+  (testing "special case for * and +"
+    (is (= (sut/process-operator {} [:* []])
+            {:raw-string "1", :params []}))
+    (is (= (sut/process-operator {} [:+ []])
+          {:raw-string "0", :params []})))
+
+  (testing "special case for - and /"
+    (is (= (sut/process-operator {} [:- [{}]])
+          {:raw-string "0-(?)", :params [{}]}))
+    (is (= (sut/process-operator {} [:/ [{}]])
+          {:raw-string "1/(?)", :params [{}]})))
+
+  (testing "normal case for + (the same to *, -, /)"
+    (is (= (sut/process-operator {} [:+ [{}]])
+          {:raw-string "(?)", :params [{}]}))
+    (is (= (sut/process-operator {} [:+ [{} {}]])
+          {:raw-string "(?)+(?)", :params [{} {}]}))
+    (is (= (sut/process-operator {} [:+ [{} {} {}]])
+          {:raw-string "(?)+(?)+(?)", :params [{} {} {}]}))))
