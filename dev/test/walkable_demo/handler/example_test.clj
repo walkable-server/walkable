36804e207851394bc2993108f99c40e6ecfe58bf
Hoàng Minh Thắng <p@banphim.net>
2018-08-11 17:27:05 +0700

    remove demo handler test

--- dev/test/walkable_demo/handler/example_test.clj
+++
@@ -1,5 +0,0 @@
-(ns walkable-demo.handler.example-test
-  (:require [clojure.test :refer :all]
-            [integrant.core :as ig]
-            [ring.mock.request :as mock]
-            [walkable-demo.handler.example :as example]))
