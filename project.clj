9ab147a7ff29714c85346fcdbffa24c81321f03f
Hoàng Minh Thắng <p@banphim.net>
2018-08-11 17:23:31 +0700

    update all project's dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "https://github.com/walkable-server/walkable"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [clojure-future-spec "1.9.0-beta4"]
-                 [com.wsscode/pathom "2.0.0" :exclusions [org.clojure/clojurescript]]]
+                 ;; not work with latest spec version yet
+                 ;;[org.clojure/spec.alpha "0.2.168"]
+                 [com.wsscode/pathom "2.0.19"]]
   :resource-paths ["resources"]
   :profiles
   {:dev          [:project/dev :profiles/dev]
@@ -18,24 +20,25 @@
    :project/dev  {:main           ^:skip-aot walkable-demo.main
                   ;; :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
                   :plugins        [[duct/lein-duct "0.10.6"]]
-                  :source-paths   ["dev/src"]
+                  :source-paths   ["dev/src" "dev/test"]
                   :resource-paths ["dev/resources" "target/resources"]
                   :dependencies   [[duct/core "0.6.2"]
                                    [duct/module.logging "0.3.1"]
                                    [duct/logger.timbre "0.4.1"]
+                                   [org.clojure/test.check "0.10.0-alpha3"]
                                    [duct/module.web "0.6.4"]
                                    [duct/module.ataraxy "0.2.0"]
                                    [cheshire "5.8.0"]
 
                                    [duct/module.sql "0.4.2"]
-                                   [duct/database.sql.hikaricp "0.3.2"]
+                                   [duct/database.sql.hikaricp "0.3.3"]
 
                                    ;; enable if you use postgresql
-                                   ;; [org.postgresql/postgresql "42.2.1"]
+                                   ;; [org.postgresql/postgresql "42.2.4"]
                                    ;; enable if you use mysql
                                    ;; [mysql/mysql-connector-java "5.1.45"]
-                                   [org.xerial/sqlite-jdbc "3.21.0.1"]
+                                   [org.xerial/sqlite-jdbc "3.23.1"]
 
-                                   [integrant/repl "0.3.0"]
-                                   [eftest "0.4.3"]
+                                   [integrant/repl "0.3.1"]
+                                   [eftest "0.5.2"]
                                    [kerodon "0.9.0"]]}})
