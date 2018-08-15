b3e336c5c97991d2bfcfacec89cf529400d1af4a
Hoàng Minh Thắng <p@banphim.net>
2018-08-16 00:29:11 +0700

    add mysql and postgres integration test ns

---
+++ dev/test/mysql/walkable/integration_test/mysql_test.clj
@@ -0,0 +1,26 @@
+(ns walkable.integration-test.mysql-test
+  (:require [walkable.sql-query-builder :as sqb]
+            [walkable.integration-test.helper :refer [run-scenario-tests]]
+            [walkable.integration-test.common :refer [common-scenarios]]
+            [clojure.java.io :as io]
+            [integrant.core :as ig]
+            [duct.core :as duct]
+            [clojure.test :as t :refer [deftest is]]
+            [com.wsscode.pathom.core :as p]
+            [clojure.java.jdbc :as jdbc]))
+
+(duct/load-hierarchy)
+
+(def system
+  (-> (duct/read-config (io/resource "config-mysql.edn"))
+    (duct/prep)
+    (ig/init)))
+
+(def db
+  (-> system (ig/find-derived-1 :duct.database/sql) val :spec))
+
+(deftest common-scenarios-test
+  (run-scenario-tests db :mysql common-scenarios))
+
+(deftest mysql-specific-scenarios-test
+  (run-scenario-tests db :mysql {}))
---
+++ dev/test/postgres/walkable/integration_test/postgres_test.clj
@@ -0,0 +1,27 @@
+(ns walkable.integration-test.postgres-test
+  (:require [walkable.sql-query-builder :as sqb]
+            [walkable.integration-test.helper :refer [run-scenario-tests]]
+            [walkable.integration-test.common :refer [common-scenarios]]
+            [clojure.java.io :as io]
+            [integrant.core :as ig]
+            [duct.core :as duct]
+            [clojure.test :as t :refer [deftest is]]
+            [com.wsscode.pathom.core :as p]
+            [clojure.java.jdbc :as jdbc]
+            [walkable.sql-query-builder.impl.postgres]))
+
+(duct/load-hierarchy)
+
+(def system
+  (-> (duct/read-config (io/resource "config-postgres.edn"))
+    (duct/prep)
+    (ig/init)))
+
+(def db
+  (-> system (ig/find-derived-1 :duct.database/sql) val :spec))
+
+(deftest common-scenarios-test
+  (run-scenario-tests db :postgres common-scenarios))
+
+(deftest postgres-specific-scenarios-test
+  (run-scenario-tests db :postgres {}))
