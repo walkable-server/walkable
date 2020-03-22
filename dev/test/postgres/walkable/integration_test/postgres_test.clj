9b10de0d4dc1903e32624e9fd518e899b36fba48
Hoàng Minh Thắng <p@banphim.net>
2020-03-23 04:47:18 +0700

    update postgres test

--- dev/test/postgres/walkable/integration_test/postgres_test.clj
+++ dev/test/postgres/walkable/integration_test/postgres_test.clj
@@ -1,14 +1,11 @@
 (ns walkable.integration-test.postgres-test
   {:integration true}
-  (:require [walkable.sql-query-builder :as sqb]
-            [walkable.integration-test.helper :refer [run-scenario-tests]]
+  (:require [walkable.integration-test.helper :refer [run-scenario-tests]]
             [walkable.integration-test.common :refer [common-scenarios]]
             [clojure.java.io :as io]
             [integrant.core :as ig]
             [duct.core :as duct]
-            [clojure.test :as t :refer [deftest is]]
-            [com.wsscode.pathom.core :as p]
-            [clojure.java.jdbc :as jdbc]
+            [clojure.test :as t :refer [deftest]]
             [walkable.sql-query-builder.impl.postgres]))
 
 (duct/load-hierarchy)
