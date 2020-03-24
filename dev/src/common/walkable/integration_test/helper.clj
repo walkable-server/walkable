75f3c67ea5338be27db9ced6f4fd83b842eefd3c
Hoàng Minh Thắng <p@banphim.net>
2020-03-24 18:50:53 +0700

    update test helper

--- dev/src/common/walkable/integration_test/helper.clj
+++ dev/src/common/walkable/integration_test/helper.clj
@@ -11,77 +11,27 @@
    :sqlite   emitter/sqlite-emitter
    :postgres emitter/postgres-emitter})
 
-(defn core-config
-  [{:keys [db-type core-floor-plan cte?]}]
-  (let [resolver-sym `my-resolver]
-    {:resolver-sym resolver-sym
-     :index-oir
-     (merge
-       {:farmers/farmers {#{} #{resolver-sym}}
-        :houses/houses  {#{} #{resolver-sym}}}
+(defn connect-config
+  [{:keys [db-type core-config core-floor-plan cte?]}]
+  (let [resolver-sym `test-resolver]
+    (merge core-config
+      {:resolver-sym resolver-sym
 
-       #:farmer{:number      {#{:farmers/farmers} #{resolver-sym}}
-                :name        {#{:farmers/farmers} #{resolver-sym}}
-                :house-index {#{:farmers/farmers} #{resolver-sym}}
-                :house       {#{:farmer/number}      #{resolver-sym}
-                              #{:farmer/house-index} #{resolver-sym}}}
-       #:house{:index        {#{:farmer/house} #{resolver-sym}
-                              #{:houses/houses}   #{resolver-sym}}
-               :color        {#{:house/index} #{resolver-sym}
-                              #{:houses/houses}  #{resolver-sym}}
-               :owner-number {#{:house/index} #{resolver-sym}
-                              #{:houses/houses}  #{resolver-sym}}
-               :owner        {#{:house/index} #{resolver-sym}
-                              #{:houses/houses}  #{resolver-sym}}})
-
-     :index-io
-     {#{}
-      {:farmers/farmers #:farmer{:number {} :name {} :house-index {} :house #:house{:index {}}}
-       :houses/houses #:house{:index {} :color {} :owner #:farmer {:number {}}}}
-
-      #{:farmer/number}
-      #:farmer{:name {} :house-index {}}
-
-      #{:farmer/house-index}
-      #:house{:index {} :color {}
-              :owner #:farmer{:number {}}}
-
-      #{:house/index}
-      #:house{:color {}
-              :owner #:farmer{:number {}}}}
-
-     :index-idents
-     #{:farmer/number :house/index}
-
-     :floor-plan
-     (merge core-floor-plan
-       {:emitter          (db-specific-emitter db-type)
-        :use-cte          (when cte? {:default true})
-        ;; columns already declared in :joins are not needed
-        ;; here
-        :true-columns     [:house/color
-                           :farmer/number
-                           :farmer/name]
-        :idents           {:farmer/id       :farmer/number
-                           :farmers/farmers "farmer"
-                           :houses/houses   "house"}
-        :extra-conditions {}
-        :joins            {:farmer/house [:farmer/house-index :house/index]}
-        :reversed-joins   {:house/owner :farmer/house}
-        :cardinality      {:farmer/id    :one
-                           :house/owner  :one
-                           :farmer/house :one}})}))
+       :floor-plan
+       (merge core-floor-plan
+         {:emitter (db-specific-emitter db-type)
+          :use-cte (when cte? {:default true})})})))
 
 (defn walkable-parser
   [{:keys [db] :as config}]
   (p/parser
     {::p/env     {::p/reader               [p/map-reader
-                                            pc/reader2
+                                            pc/reader3
                                             pc/open-ident-reader
                                             p/env-placeholder-reader]
                   ::p/placeholder-prefixes #{">"}}
      ::p/plugins [(pc/connect-plugin {::pc/register []})
-                  (walkable/connect-plugin (assoc (core-config config)
+                  (walkable/connect-plugin (assoc (connect-config config)
                                              :db db
                                              :query jdbc/query))
                   p/error-handler-plugin
@@ -90,17 +40,19 @@
 (defn run-scenario-tests
   [db db-type scenarios]
   (into []
-    (for [[scenario {:keys [core-floor-plan test-suite]}] scenarios
+    (for [[scenario {:keys [core-floor-plan core-config test-suite]}] scenarios
           {:keys [message env query expected]}            test-suite]
       (testing (str "In scenario " scenario " for " db-type ", testing " message)
         (is (= expected
               (let [parser (walkable-parser {:db db
+                                             :core-config core-config
                                              :core-floor-plan core-floor-plan
                                              :db-type db-type})]
                 (parser (assoc env ::p/placeholder-prefixes #{"ph"}) query)))
           "without CTEs in joins")
         (is (= expected
               (let [parser (walkable-parser {:db db
+                                             :core-config core-config
                                              :core-floor-plan core-floor-plan
                                              :db-type db-type
                                              :cte? true})]
