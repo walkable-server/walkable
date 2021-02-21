cc264dfdc7f10d9944757d41ab4ca78bbb7f55be
Hoàng Minh Thắng <p@banphim.net>
2021-02-21 13:10:29 +0700

    update dev.cljs

--- dev/src/common/dev.cljs
+++ dev/src/common/dev.cljs
@@ -1,42 +1,16 @@
 (ns dev
   (:require [com.wsscode.pathom.core :as p]
             [com.wsscode.pathom.connect :as pc]
+            [com.wsscode.pathom.connect.planner :as pcp]
             [walkable.core-async :as walkable]
             [walkable.sql-query-builder.emitter :as emitter]
+            [walkable.sql-query-builder.floor-plan :as floor-plan]
             [walkable.integration-test.common :as common]
             ["sqlite3" :as sqlite3]
-            [walkable.sql-query-builder.impl.sqlite]
             [cljs.core.async :as async :refer [put! >! <! promise-chan]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
-
-(defn connect-config
-  [{:keys [core-config core-floor-plan cte?]}]
-  (let [resolver-sym `test-resolver]
-    (merge core-config
-           {:resolver-sym resolver-sym
-            :floor-plan
-            (merge core-floor-plan
-                   {:emitter emitter/sqlite-emitter
-                    :use-cte (when cte? {:default true})})})))
-
-(defn walkable-parser
-  [config]
-  (p/async-parser
-   {::p/env {::p/reader [p/map-reader
-                         pc/reader3
-                         pc/open-ident-reader
-                         p/env-placeholder-reader]
-             ::p/process-error
-             (fn [_ err]
-               (js/console.error err)
-               (p/error-str err))}
-    ::p/plugins [(pc/connect-plugin {::pc/register []})
-                 (walkable/connect-plugin (connect-config config))
-                 p/elide-special-outputs-plugin
-                 p/error-handler-plugin
-                 p/trace-plugin]}))
 
 (def db (sqlite3/Database. "walkable_dev.sqlite"))
 
@@ -52,35 +26,38 @@
           (put! c x))))
     c))
 
-(def common-env
-  {::walkable/db db ::walkable/run async-run-print-query})
+(defn walkable-parser
+  [db-type registry]
+  (p/async-parser
+   {::p/env {::p/reader [p/map-reader
+                         pc/reader3
+                         pc/open-ident-reader
+                         p/env-placeholder-reader]
+             ::p/process-error
+             (fn [_ err]
+               (js/console.error err)
+               (p/error-str err))}
+    ::p/plugins [(pc/connect-plugin {::pc/register []})
+                 (walkable/connect-plugin {:db-type db-type
+                                           :registry registry
+                                           :query-env #(async-run-print-query (:db %1) %2)})
+                 p/elide-special-outputs-plugin
+                 p/error-handler-plugin
+                 p/trace-plugin]}))
 
-;; all queries have been comment out
-;; uncomment them one by one to see them in action
+(def w* (walkable-parser :sqlite common/person-pet-registry))
 
-(let [q-1
-      '[{#_ :farmers/farmers
-         [:farmer/number 1]
-         [:farmer/number :farmer/name
-          {:farmer/house [:house/index :house/color]}]}]
+(defn w
+  [q]
+  (w* {:db db} q))
 
-      parser
-      (walkable-parser {:core-floor-plan common/farmer-house-floor-plan
-                        :core-config common/farmer-house-config})]
-  (go
-    (println "\n\n<-- final result -->\n"
-             (<! (parser common-env q-1)))))
+(comment
+  (w `[{(:pets/by-color {:order-by [:pet/color :desc]})
+        [:pet/color]}])
 
-(let [q-2
-      '[{[:kid/number 1] [:kid/number :kid/name
-                         {:kid/toy [:toy/index :toy/color]}]}]
-
-      parser (walkable-parser {:core-floor-plan common/kid-toy-floor-plan
-                               :core-config common/kid-toy-config})]
-  (println "\n\n")
-  (go
-    (println "final result"
-             (<! (parser common-env q-2)))))
+  (w `[(:people/count {:filter [:and {:person/pet [:or [:= :pet/color "white"]
+                                                   [:= :pet/color "yellow"]]}
+                                [:< :person/number 10]]})]))
 
 (defn main []
-  (println "<-- Walkable for nodejs demo -->"))
+  (println "walkable runs in nodejs!!!"))
