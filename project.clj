dd29bd5da1b090b6feff5cc67f5b15990ff60847
Hoàng Minh Thắng <p@banphim.net>
2021-08-01 23:45:50 +0700

    fix indent in project.clj

--- project.clj
+++ project.clj
@@ -29,14 +29,14 @@
                  :test-paths     ["dev/test"]
                  :resource-paths ["dev/resources"]
                  :dependencies   [[duct/core "0.8.0"]
-                                [duct/module.logging "0.5.0"]
-                                [duct/module.sql "0.6.0"]
-                                [org.clojure/test.check "1.0.0"]
-                                [cheshire "5.10.0"]
-                                [integrant/repl "0.3.1"]
-                                [eftest "0.5.9"]
-                                [kerodon "0.9.1"]
-                                ;; sql flavors
-                                [org.xerial/sqlite-jdbc "3.31.1"]
-                                [org.postgresql/postgresql "42.2.12"]
-                                [mysql/mysql-connector-java "8.0.20"]]}})
+                                  [duct/module.logging "0.5.0"]
+                                  [duct/module.sql "0.6.0"]
+                                  [org.clojure/test.check "1.0.0"]
+                                  [cheshire "5.10.0"]
+                                  [integrant/repl "0.3.1"]
+                                  [eftest "0.5.9"]
+                                  [kerodon "0.9.1"]
+                                  ;; sql flavors
+                                  [org.xerial/sqlite-jdbc "3.31.1"]
+                                  [org.postgresql/postgresql "42.2.12"]
+                                  [mysql/mysql-connector-java "8.0.20"]]}})
